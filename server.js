f5bd21e8167b3df41f5d6ce887545b648678bc1c
neha <nehasonowal1008@gmail.com>
2025-05-17 10:32:29 +0530

    done

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log( err));
+//schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    subject: String,
+    message: String
+});
+
+const User = mongoose.model('Contact', ContactSchema);
+
+app.post('/submit-form', async (req, res) => {
+    const { name, email, subject, message } = req.body;
+
+    try{
+       const newMessage = new Contact({name, email, subject, message});
+       await newMessage.save();
+       res.status(200).json({message: 'Message submitted successfully'});
+    }catch(error){
+        res.status(500).json({message: 'Something went wrong'});
+    }
+});
+//server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+    
